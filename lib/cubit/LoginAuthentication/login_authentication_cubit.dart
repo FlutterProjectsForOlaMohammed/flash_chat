0fedcaacca8c9fafd654ea728de766b4ad1e2983
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-05-12 18:40:40 +0300

    final adjustment for flash chat app (v1)

--- lib/cubit/LoginAuthentication/login_authentication_cubit.dart
+++ lib/cubit/LoginAuthentication/login_authentication_cubit.dart
@@ -1,9 +1,6 @@
-import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/contants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 part 'login_authentication_state.dart';
 
@@ -67,21 +64,5 @@ class LoginAuthenticationCubit extends Cubit<AuthenticationState> {
 
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
-  }
-
-  Future<UserCredential> loginWithFacebook() async {
-    // Trigger the sign-in flow
-    final LoginResult loginResult = await FacebookAuth.instance.login(
-      permissions: [
-        'email',
-        'public_profile',
-        'user_birthday',
-      ],
-    );
-    // Create a credential from the access token
-    final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
-    // Once signed in, return the UserCredential
-    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 }
